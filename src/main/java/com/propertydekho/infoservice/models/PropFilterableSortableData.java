cb5fc060a4c7ce6a77fe84b28047f6fa68427226
Divsjains6021 <divsjains6021@gmail.com>
2020-05-18 00:19:42 +0530

    created utilities file to put data in database

--- src/main/java/com/propertydekho/infoservice/models/PropFilterableSortableData.java
+++ src/main/java/com/propertydekho/infoservice/models/PropFilterableSortableData.java
@@ -13,8 +13,8 @@ public class PropFilterableSortableData {
     private String propName;
     @JsonProperty("prop_price")
     private double propPrice;
-    @JsonProperty("budget_max")
-    private double budgetMax;
+    @JsonProperty("sq_ft")
+    private int sqft;
     @JsonProperty("bedroom")
     private String bedroom;
     @JsonProperty("sale_type")
@@ -28,14 +28,18 @@ public class PropFilterableSortableData {
 
     }
 
-    public PropFilterableSortableData(String propID, String propName, double propPrice, double budgetMax, String bedroom, String saleType, String constructionStatus, String area) {
+    public PropFilterableSortableData(String propID, String propName, double propPrice, int sqft, String bedroom, String saleType, String constructionStatus, String area) {
         this.propID = propID;
         this.propName = propName;
         this.propPrice = propPrice;
-        this.budgetMax = budgetMax;
+        this.sqft = sqft;
         this.bedroom = bedroom;
         this.saleType = saleType;
         this.constructionStatus = constructionStatus;
         this.area = area;
+    }
+
+    public String getPropName() {
+        return getBedroom() + " " + getSqft() + " Sq-ft Flat for Sale in " + getArea();
     }
 }
